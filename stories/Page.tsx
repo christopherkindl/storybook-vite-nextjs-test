5851fbe5245232032c741fd721c19339c2b9146d
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2025-06-17 12:37:40 +0200

    fix: clean up stories

--- stories/Page.tsx
+++ stories/Page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Header } from "./Header";
-
 type User = {
   name: string;
 };
@@ -10,19 +8,10 @@ export const Page: React.FC = () => {
   const [user, setUser] = React.useState<User>();
 
   return (
-    <article>
-      <Header
-        user={user}
-        onLogin={() => setUser({ name: "Jane Doe" })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: "Jane Doe" })}
-      />
-
-      <section className="p-10">
-        <h2 className="text-blue-900 text-2xl">
-          This is a test to see whether Tailwind works
-        </h2>
-      </section>
-    </article>
+    <section className="p-10">
+      <h2 className="text-blue-900 text-2xl">
+        This is a test to see whether Tailwind works
+      </h2>
+    </section>
   );
 };
